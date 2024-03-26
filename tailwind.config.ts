1197f044e94eaf0d556a8818cb521728525dc5da
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-03-27 02:32:26 +0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary': '#303633',
+      'secondary': '#8CE8CB',
+      'accent': '#8CE8CB',
+
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+
       },
     },
   },
